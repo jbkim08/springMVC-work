c9e3fceea0a0616a3a84223e374b7e2288ae37e8
jbkim <drv98@naver.com>
2022-10-25 11:43:49 +0900

    요청메서드

--- URLMapping/WebContent/WEB-INF/views/index.jsp
+++ URLMapping/WebContent/WEB-INF/views/index.jsp
@@ -11,5 +11,16 @@
 	<a href="test2">테스트2요청</a><br>
 	<a href="sub1/test3">sub1/test3</a><br>
 	<a href="sub1/test4">sub1/test4</a><br>
+	<form action="test1" method="post">
+		<button type="submit">test1 post</button>
+	</form>
+	<form action="test2" method="post">
+		<button type="submit">test2 post</button>
+	</form>
+	<a href="test5">test5</a><br>
+	<form action="test5" method="post">
+		<button type="submit">test5 post</button>
+	</form>
+	
 </body>
 </html>
--- URLMapping/src/com/demo/controller/TestController.java
+++ URLMapping/src/com/demo/controller/TestController.java
@@ -2,19 +2,43 @@ package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 
 	@GetMapping("/test1")
-	public String test1() {
+	public String test1_get() {
 		return "test1";
+	}
+	
+	@PostMapping("/test1")
+	public String test1_post() {
+		return "post1";
 	}
 	
 	@GetMapping("/test2")
 	public String test2() {
 		return "test2";
 	}
+	
+	@PostMapping("/test2")
+	public String test2_post() {
+		return "post2";
+	}
+	
+	@RequestMapping(value = "/test5", method = {RequestMethod.GET, RequestMethod.POST})
+	public String test5() {
+		return "test5";
+	}
 }
+
+
+
+
+
+
 
 
