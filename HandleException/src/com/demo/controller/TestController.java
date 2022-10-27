3ff287e5f6f67878b641d8f3fc6a76d07017a530
jbkim <drv98@naver.com>
2022-10-27 09:29:58 +0900

    마이바티스1

--- HandleException/src/com/demo/controller/TestController.java
+++ HandleException/src/com/demo/controller/TestController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
---
+++ MyBatis/src/com/demo/mapper/MapperInterface.java
@@ -0,0 +1,5 @@
+package com.demo.mapper;
+
+public interface MapperInterface {
+	//sql 쿼리문 작성
+}
